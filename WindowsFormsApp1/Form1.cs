d030fa3a2ea3b84045b0065b629eebcdb301f717
Krivets <bi50_i.m.krivec@mail.ru>
2020-03-04 00:46:00 +0300

    MetroFramework

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.StyleManager = Calculator;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                int a, b;
+                a = Convert.ToInt32(textBox1.Text);
+                b = Convert.ToInt32(textBox2.Text);
+                textBox1.Text = "";
+                textBox2.Text = "";
+                a = a + b;
+                label1.Text = Convert.ToString(a);
+            }
+            else
+
+            {
+                MessageBox.Show(text: "Для использованиия калькулятора нажмите на галочку");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                if (checkBox1.Checked == true)
+                {
+                    int a, b;
+                    a = Convert.ToInt32(textBox1.Text);
+                    b = Convert.ToInt32(textBox2.Text);
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    a = a - b;
+                    label1.Text = Convert.ToString(a);
+                }
+                else
+
+                {
+                    MessageBox.Show(text: "Для использованиия калькулятора нажмите на галочку");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+                if (checkBox1.Checked == true)
+                {
+                    int a, b;
+                    a = Convert.ToInt32(textBox1.Text);
+                    b = Convert.ToInt32(textBox2.Text);
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    a = a * b;
+                    label1.Text = Convert.ToString(a);
+                }
+                else
+
+                {
+                    MessageBox.Show(text: "Для использованиия калькулятора нажмите на галочку");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            {
+                if (checkBox1.Checked == true)
+
+                {
+                    int a, b;
+                    a = Convert.ToInt32(textBox1.Text);
+                    b = Convert.ToInt32(textBox2.Text);
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    if (b == 0)
+                        MessageBox.Show(text: "Делить на ноль нелязя!");
+                    else
+                    {
+                        a = a / b;
+                        label1.Text = Convert.ToString(a);
+                    }
+                }
+
+                else
+
+                {
+                    MessageBox.Show(text: "Для использованиия калькулятора нажмите на галочку");
+                }
+            }
+        }
+    }
+}
